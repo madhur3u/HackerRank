0995d7375fe9a6eafe12223fc334e242b27f544c
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-05 15:55:57 +0530

    Update NumberLineJump.py

--- NumberLineJump.py
+++ NumberLineJump.py
@@ -5,17 +5,14 @@ x1,v1,x2,v2 = arr
 a = True
 if (x1 == x2) : 
     print('YES')
-    a = False
+    exit(0)
+if (v2 > v1 or v2 == v1) :
+    print('NO')
+    exit(0)
 while a :
     x1 = x1 + v1
     x2 = x2 + v2
     if x1>x2 and v1>v2 :
-        print('NO')
-        break
-    if x2>x1 and v2>v1 :
-        print('NO')
-        break
-    if x2!=x1 and v2==v1 :
         print('NO')
         break
     if x1 == x2 :
