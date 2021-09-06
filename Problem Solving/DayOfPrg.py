d5ad0afc6792f2cf7676c11c29706dca551812f1
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-06 11:48:51 +0530

    Create DayOfPrg.py

---
+++ Problem Solving/DayOfPrg.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/day-of-the-programmer/leaderboard
+
+n = int(input())
+if n < 1918 :
+    if n % 4 == 0 : print('12.09.' + str(n))
+    else : print('13.09.' + str(n))
+
+elif n == 1918 : 
+    print('26.09.' + str(n))
+
+else :
+    if n % 400 == 0 : 
+        print('12.09.' + str(n))
+    elif n % 4 == 0 and n % 100 != 0 : 
+        print('12.09.' + str(n))
+    else : 
+        print('13.09.' + str(n))
