1ec2ce12b75c1355abcebe847cae4c479349672f
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-06 22:41:07 +0530

    Create Find_Point.py

---
+++ Mathematics/Fundamentals/Find_Point.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/find-point/leaderboard
+
+n = int(input())
+for i in range(n) :
+    
+    p1, p2, q1, q2 = map(int, input().split())
+    print(2*q1 - p1, 2*q2 - p2)
