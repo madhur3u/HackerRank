0aeb4d26412f80461ac13188951e97d4b72eaad8
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-07 11:39:24 +0530

    Create Max_Draws.java

---
+++ Mathematics/Fundamentals/Max_Draws.java
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/maximum-draws/problem
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        
+        for (int t1 = 0; t1 < t; t1++){
+            int n = scn.nextInt();
+            System.out.println(n + 1);
+        }
+    }
+}
