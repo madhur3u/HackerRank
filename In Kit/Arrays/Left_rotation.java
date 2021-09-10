eae64a4e26b3ac572d0706d17ee842280f2a5aa3
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-10 14:02:14 +0530

    Create Left_rotation.java

---
+++ In Kit/Arrays/Left_rotation.java
@@ -0,0 +1,38 @@
+/* Sample Input
+5 4
+1 2 3 4 5
+Sample Output
+5 1 2 3 4
+
+https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++){
+            a[i] = scn.nextInt();      // input array
+        }
+
+        for (int i = 0; i < n; i++){
+            System.out.print(a[(i+k)%n] + " ");
+        }
+
+    }
+}
+
+/* PYTHON PROGRAM 
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+for i in a[k:n] : print(i, end=' ')
+for j in a[0:k] : print(j, end=' ')
+*/
