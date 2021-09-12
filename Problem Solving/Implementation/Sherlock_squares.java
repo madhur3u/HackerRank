e03413af1bfbd7f1873f9f04914b4550fdf0ba7d
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-12 12:29:06 +0530

    Create Sherlock_squares.java

---
+++ Problem Solving/Implementation/Sherlock_squares.java
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int q = scn.nextInt();
+
+        for (int q1 = 1; q1<=q; q1++){
+
+            int a = scn.nextInt();
+            int b = scn.nextInt();
+            int a1 = (int)Math.sqrt(a);
+            int b1 = (int)Math.sqrt(b);
+            int i = a1;
+
+        //    for (i = i ; i <= b1; i++) System.out.println(i*i);  //to print squares in that range
+            int c = b1 - a1;
+            if (a == a1*a1) c++;
+
+            System.out.println(c);
+
+        }
+    }
+}
