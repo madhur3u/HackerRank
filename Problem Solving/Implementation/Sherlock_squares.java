2eba3a42c7a9f1d4a206ebc9361d083d3b96c2bd
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-12 12:32:19 +0530

    Update Sherlock_squares.java

--- Problem Solving/Implementation/Sherlock_squares.java
+++ Problem Solving/Implementation/Sherlock_squares.java
@@ -26,3 +26,14 @@ public class Solution {
         }
     }
 }
+
+/* alt python code 
+
+import math
+t = input()
+for _ in range(t):
+    a, b = map(int, input().split())
+    a = ceil(sqrt(a))
+    b = floor(sqrt(b))
+    print int(b-a) + 1
+*/
