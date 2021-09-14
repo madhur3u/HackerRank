683890736c3d801f9bca81f9012e7c3a148fc140
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-14 20:05:08 +0530

    Update sol_main.py

--- Mathematics/Fundamentals/Bus Station/sol_main.py
+++ Mathematics/Fundamentals/Bus Station/sol_main.py
@@ -6,18 +6,20 @@ n = int(input())
 a = list(map(int, input().split()))
 b = []
 x = sum(a)
-for i in range(1,int(math.sqrt(x)) + 1): # efficient method to find factors
+for i in range(1,int(math.sqrt(x)) + 1): 
     if(x % i == 0) : 
         b.append(i)
         if(i != x/i) : b.append(x//i) # for perfect sq we may get i and x/i same so this will ensure only one value go in list
 
 b.sort() # now b has all factors from 1 to x
 
-# what we are doing here (above) is finding factors of sum of a as bus can never be empty so the ans will atleast be a factor
+# what we are doing here (above) is finding factors of sum of (a) as bus can never be empty so the ans will atleast be a factor
 # of sum(a). we have to sort the factors as ans should be printed in inc order
 
 
 for lbus in b:  
+    if lbus < max(a) : continue # it should be at bigger than max(a) else the bus can never be able to accomodate that group
+
     s = 0
     for i in range(n):
         s = s + a[i] # sum of all groups till i
@@ -25,7 +27,6 @@ for lbus in b:
             s = 0
         
         if (s > lbus) : # if sum become greater that ln is not acceptable so break 
-            c = 0
             break
     
     if (s == 0) : print(lbus,end =" ") # if s = 0 then it is value as inside 1st if we are doind s = 0 if lbus is the one
