3a511941fd9b4ae777029b2fdeb3569f4af4fee5
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-18 17:52:57 +0530

    Update real_sol.py

--- Mathematics/Fundamentals/Matrix Tracing/real_sol.py
+++ Mathematics/Fundamentals/Matrix Tracing/real_sol.py
@@ -23,3 +23,13 @@ for i in range(0,T):
     m = int(m)
     n = int(n)
     print(solve(m, n))
+    
+''' The answer is that we have (N-1+M-1)!/((N-1)!(M-1)!) different paths for given M and N. 
+For example for the sample input, we have (2-1+3-1)!/((2-1)!(3-1)!) = 3!/(1*2) which is 6/2= 3.
+This is because there will be M-1 moves RIGHT and N-1 moves DOWN.
+(m+n-2)(C)(m-1). 
+Therefore for your example we can have (for 3x3)
+RIGHT DOWN RIGHT
+RIGHT RIGHT DOWN
+DOWN RIGHT RIGHT
+So these are just the permutations of M-1 RIGHT and N-1 DOWN '''
