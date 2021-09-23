f5ee62ac5e0b9b41cc50abb4c5c42bd11bacbc91
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-23 18:36:09 +0530

    Update Diwali_lights.py

--- Mathematics/Fundamentals/Diwali_lights.py
+++ Mathematics/Fundamentals/Diwali_lights.py
@@ -4,7 +4,17 @@ q = int(input())
 for x in range(q):
     n = int(input())
     
-    sum1 = 2**n - 1 # sum of all combinations of n
+    sum1 = 2**n - 1             # sum of all combinations of n
     print(sum1 % 100000)
+    
+# for 3 we have these combinations
+# *   -   -
+# -   *   -
+# -   -   * 
+# *   *   -
+# *   -   *
+# -   *   *
+# *   *   *
+# we have 7 combination here 2**3 is 8 since we never take  0 0 0 here thats why 2**n - 1
 
 # for java BITINTEGER to be used in this q    
