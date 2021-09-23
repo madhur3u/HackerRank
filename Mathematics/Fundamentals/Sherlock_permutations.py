96cbcea3f030a8b12c60e3039df7295b1595bc04
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-23 18:47:19 +0530

    Update Sherlock_permutations.py

--- Mathematics/Fundamentals/Sherlock_permutations.py
+++ Mathematics/Fundamentals/Sherlock_permutations.py
@@ -1,5 +1,8 @@
 # https://www.hackerrank.com/challenges/sherlock-and-permutations/problem
 
+# We fix a 1 at the first position. Now we are left with M-1 1s and N 0s. Now we can just count unique permutations 
+# of (M-1) 1s and N 0s, which is choose((M+N-1), N), which is (M+N-1)!/(N! * (M-1)!)
+
 def fact(n) :
     f = 1
     for i in range(2,n+1): f = f*i
