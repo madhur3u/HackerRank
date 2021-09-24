691d43b5b91e4597678471b5e58ca7bbb571e5a5
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-24 20:23:36 +0530

    Update Special_multiple.py

--- Mathematics/Fundamentals/Special_multiple.py
+++ Mathematics/Fundamentals/Special_multiple.py
@@ -1,16 +1,28 @@
 # https://www.hackerrank.com/challenges/special-multiple/problem
 
-def db(n): # func for decimal to binary
+def db(n):                               # func for decimal to binary
     return int("{0:b}".format(int(n)))
 
 a = []
-for i in range(1,5000): # this create a array of 1st 5000 numbers like we need 
+for i in range(1,5000):                  # this create a array of 1st 5000 numbers like we need 
     a.append(db(i)*9)
     
 for q in range(int(input())):
     n = int(input())
-    for i in a :
-        x = i
-        if(i % n == 0) : break
+    for i in a :                         # iterating in our arrays of 9's 
+        x = i                            # store value temporarily in x 
+        if(i % n == 0) : break           # if the i is divisible bt N so we found our answer
         
     print(x)
+
+# binary digits 1-8
+# 000 
+# 001
+# 010
+# 011
+# 100
+# 101
+# 110
+# 111
+
+# so we can find binary of a number and multiply it by 9 so we will get all combinations of number with 0-9 till 5000 here
